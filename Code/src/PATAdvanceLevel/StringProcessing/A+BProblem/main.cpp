7366fa0a3fed40a34f6b521d7544083695df7e45
yangning <2428022854@qq.com>
2023-03-03 17:11:08 +0800

    revise A+Bproblem

--- Code/src/PATAdvanceLevel/StringProcessing/A+BProblem/main.cpp
+++ Code/src/PATAdvanceLevel/StringProcessing/A+BProblem/main.cpp
@@ -1,3 +1,19 @@
 //
 // Created by 阳寜 on 2023/3/3.
 //
+#include "iostream"
+using namespace std;
+int main() {
+    int a,b;
+    cin >> a >> b;
+
+    string x = to_string(a+b);
+    string res = "";
+    for (int i = x.size() -1 ,j=0; i >=0; i--) {
+        j++;
+        res = x[i] + res;
+
+        if (j%3 == 0 && i && x[i-1] != '-') res = "," + res;
+    }
+    cout << res << endl;
+}
