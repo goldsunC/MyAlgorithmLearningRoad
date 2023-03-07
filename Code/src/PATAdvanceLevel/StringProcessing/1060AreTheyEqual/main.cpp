b4b236d85b69595842c00cfd2c60b99917e08fdd
yangning <2428022854@qq.com>
2023-03-07 19:05:41 +0800

    add new problem

---
+++ Code/src/PATAdvanceLevel/StringProcessing/1060AreTheyEqual/main.cpp
@@ -0,0 +1,16 @@
+//
+// Created by 阳寜 on 2023/3/7.
+//
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int N;
+    string A,B;
+    cin >> N >> A >> B;
+    
+
+
+    return 0;
+}
