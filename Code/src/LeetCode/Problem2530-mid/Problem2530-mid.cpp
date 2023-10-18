e94f109d853899029eef3b57014b713dcc0c506b
yangning <2428022854@qq.com>
2023-10-18 21:37:36 +0800

    2023/10/18 p2030-mid commit

--- Code/src/LeetCode/Problem2530-mid/Problem2530-mid.cpp
+++ Code/src/LeetCode/Problem2530-mid/Problem2530-mid.cpp
@@ -5,6 +5,22 @@
 
 using namespace std;
 
+class Solution {
+public:
+    long long maxKelements(vector<int>& nums, int k) {
+        priority_queue<int> q(nums.begin(), nums.end());
+        long long ans = 0;
+        for (int _ = 0; _ < k; ++_) {
+            int x = q.top();
+            q.pop();
+            ans += x;
+            q.push((x + 2) / 3);
+        }
+        return ans;
+    }
+};
+
+
 int main() {
     cout << "hello world!" <<endl;
     return 0;
