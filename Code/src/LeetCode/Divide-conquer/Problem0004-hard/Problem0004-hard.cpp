866a787e82fd56b02cfe13364200756a9b3e5bf7
yangning <2428022854@qq.com>
2023-10-23 15:41:43 +0800

    commit 0493

---
+++ Code/src/LeetCode/Divide-conquer/Problem0004-hard/Problem0004-hard.cpp
@@ -0,0 +1,25 @@
+//
+// Created by 阳寜 on 2023/10/23.
+//
+#include<bits/stdc++.h>
+
+using namespace std;
+
+//给定两个大小分别为 m 和 n 的正序（从小到大）数组 nums1 和 nums2。请你找出并返回这两个正序数组的 中位数 。
+//
+//算法的时间复杂度应该为 O(log (m+n)) 。
+
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        
+    }
+};
+
+
+
+
+int main(){
+    cout <<"hello world"<<endl;
+    return 0;
+}
---
+++ Code/src/LeetCode/Divide-conquer/Problem0493-hard/Problem0493-hard.cpp
@@ -0,0 +1,22 @@
+//
+// Created by 阳寜 on 2023/10/23.
+//
+#include<bits/stdc++.h>
+
+using namespace std;
+
+//给定一个数组 nums ，如果 i < j 且 nums[i] > 2*nums[j] 我们就将 (i, j) 称作一个重要翻转对。
+//
+//你需要返回给定数组中的重要翻转对的数量。
+
+class Solution {
+public:
+    int reversePairs(vector<int>& nums) {
+
+    }
+};
+
+int main(){
+
+    return 0;
+}
