568a9d64739491de7b10adefdcd2803b6c6cfe3d
yangning <2428022854@qq.com>
2023-10-25 23:55:07 +0800

    commit TsinghuaOJDataStruct1-Range

--- Code/src/TsinghuaOJ/DataStruct1/ProgrammingAssignment1/Range.cpp
+++ Code/src/TsinghuaOJ/DataStruct1/ProgrammingAssignment1/Range.cpp
@@ -2,33 +2,45 @@
 // Created by 阳寜 on 2023/10/24.
 //
 #include<iostream>
-#include <stdio.h>
-#include <stdlib.h>
-// TODO:案例过了一半，55分，需要继续改进
-int intervalNum(int pointArray[],int arrayNum,int left,int right) {
-    int k = 0;
-    for (int i = 0; i < arrayNum; ++i) {
-        if (pointArray[i] >= left && pointArray[i] <= right) k++;
-    }
-    return k;
+#include <cstdio>
+#include <cstdlib>
+/* TODO:案例过了一半，55分，需要继续改进
+ * 分析原因：忘了给输入的数轴点排序了，给出的示例是有序的，就默认有序了
+ * 算法时间复杂度不够，每次查询区间点个数都是遍历，时间复杂度总共为O(n^2)
+ * 可优化为二分查找区间端点的位置，进而计算中间元素个数
+ * 因为不让用STL模板，这里使用qsort排序
+ */
+//qsort比较函数
+int compare(const void *a, const void *b) {
+    return *(int*)a - *(int*)b;
 }
-
+int binSearch(const int *a,int left, int right, int n) {
+    while (left < right) {
+        int mid = (left + right) >> 1;
+        (n < a[mid]) ? right = mid :left = mid + 1;
+    }
+    return --left;
+}
 int main() {
 
 using namespace std;
     int n,m;
-    cin >> n >> m;
+    int a,b;
+    scanf("%d %d", &n, &m);
     int pointArray[n];
-    int interval[m][2];
     for (int i = 0; i < n; ++i) {
-        cin >> pointArray[i];
+        scanf("%d" , &pointArray[i]);
     }
+    qsort(pointArray, n, sizeof(int), compare);
     for (int i = 0; i < m; ++i) {
-        cin >> interval[i][0] >> interval[i][1];
+        scanf("%d %d", &a, &b);
+        int left = binSearch(pointArray, 0, n ,a);
+        int right = binSearch(pointArray, 0 ,n, b);
+        if (pointArray[left] == a && left >= 0) {
+            left--;
+        }
+        printf("%d\n", right - left);
     }
 
-    for (int i = 0; i < m; ++i) {
-        cout << intervalNum(pointArray,n,interval[i][0],interval[i][1]) <<endl;
-    }
     return 0;
 }
