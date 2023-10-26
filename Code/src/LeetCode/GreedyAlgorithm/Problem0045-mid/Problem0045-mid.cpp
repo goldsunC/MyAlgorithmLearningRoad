028f1ff4d3ec85c9fa0cd317dd56bd843b79375a
yangning <2428022854@qq.com>
2023-10-26 18:23:57 +0800

    commit P0045

---
+++ Code/src/LeetCode/GreedyAlgorithm/Problem0045-mid/Problem0045-mid.cpp
@@ -0,0 +1,41 @@
+//
+// Created by 阳寜 on 2023/10/26.
+//
+#include<bits/stdc++.h>
+
+using namespace std;
+
+/*
+ * 跳跃游戏2：题目0055要求判断是否能跳跃到最后，此题测试用例均能跳跃到最后，
+ * 需要求得最小条约次数
+ *
+ * 思路：经过用笔比比划划，一个思路是参考0055，每次维护一个可到达的最远距离，
+ * 而可以经某结点到达最远距离的那个结点，便是跳跃最少次数必须经过的结点。
+ * 具体实现不好做，参考了力扣题解，从左到右遍历数组，到达边界时，更新边界。并
+ * 将条约次数增加1。边界即当前能够到达的最大下标位置。
+ * 由此，记录必须经过的节点数即可得到答案。
+ * */
+
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int maxPos = 0,n = nums.size(),end=0, ans = 0;
+        for (int i = 0; i < n - 1; ++i) {
+            if (maxPos >= i) {
+                maxPos = max(maxPos, i+nums[i]);
+                if (i == end) {
+                    end = maxPos;
+                    ++ans;
+                }
+            }
+        }
+        return ans;
+    }
+};
+
+int main() {
+    Solution solution;
+    vector<int> nums = {7,0,9,6,9,6,1,7,9,0,1,2,9,0,3};
+    cout << solution.jump(nums);
+    return 0;
+}
