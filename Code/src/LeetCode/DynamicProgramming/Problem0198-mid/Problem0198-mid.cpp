c5be0a9506ac35a0bb31ee9fb193b1fd8e1d5bcb
yangning <2428022854@qq.com>
2023-11-01 19:32:51 +0800

    commit P0213 P0198

---
+++ Code/src/LeetCode/DynamicProgramming/Problem0198-mid/Problem0198-mid.cpp
@@ -0,0 +1,28 @@
+//
+// Created by 阳寜 on 2023/11/1.
+//
+#include<bits/stdc++.h>
+
+using namespace std;
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        if (nums.empty()) {
+            return 0;
+        }
+        int size = nums.size();
+        if (size == 1) {
+            return nums[0];
+        }
+        vector<int> dp = vector<int>(size, 0);
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]);
+        for (int i = 2; i < size; i++) {
+            dp[i] = max(dp[i - 2] + nums[i], dp[i - 1]);
+        }
+        return dp[size - 1];
+    }
+};
+int main() {
+    return 0;
+}
