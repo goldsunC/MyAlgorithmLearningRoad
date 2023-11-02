98873bc5aaaa5d31c313451bab406862f46af795
yangning <2428022854@qq.com>
2023-11-02 15:56:18 +0800

    commit BFS DFS

---
+++ Code/src/LeetCode/BFS/Problem0110-easy/Problem0110-easy.cpp
@@ -0,0 +1,46 @@
+//
+// Created by 阳寜 on 2023/11/2.
+//
+#include<bits/stdc++.h>
+//给定一个二叉树，判断它是否是高度平衡的二叉树。
+//本题中，一棵高度平衡二叉树定义为：
+//一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过 1 。
+using namespace std;
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+class Solution {
+public:
+    int height (TreeNode* root) {
+        if (root == NULL) {
+            return 0;
+        } else {
+            return max(height(root->left), height(root->right)) + 1;
+        }
+    }
+    bool isBalanced(TreeNode* root) {
+        if (root == NULL) return true;
+        return abs(height(root->left) - height(root->right)) <=1 && isBalanced(root->left) && isBalanced(root->right);
+    }
+};
+int main() {
+
+
+    return 0;
+}
