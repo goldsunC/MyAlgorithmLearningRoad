a0d31ddc65a40380eaf57eb1b7574726da5886ec
yangning <2428022854@qq.com>
2023-11-12 00:17:46 +0800

    rewrite P0102

--- Code/src/LeetCode/BFS/Problem0102-mid/Problem0102-mid.cpp
+++ Code/src/LeetCode/BFS/Problem0102-mid/Problem0102-mid.cpp
@@ -22,28 +22,24 @@ struct TreeNode {
 class Solution {
 public:
     vector<vector<int>> levelOrder(TreeNode* root) {
-        vector<vector<int>> ret;
+        vector <vector <int>> ret;
         if (!root) {
             return ret;
         }
-        //队列
-        queue<TreeNode*> q;
-        //先把根节点放进队列
+
+        queue <TreeNode*> q;
         q.push(root);
         while (!q.empty()) {
-            //计算当前队列长度
             int currentLevelSize = q.size();
-            ret.push_back(vector<int>());
-            for (int i = 0; i < currentLevelSize; ++i) {
-                auto node = q.front();
-                q.pop();
-                //ret.back()表示对ret中最后一个元素的引用，因为前边
-                //在ret中push了一个新vector，所以引用是那个空vector
+            ret.push_back(vector <int> ());
+            for (int i = 1; i <= currentLevelSize; ++i) {
+                auto node = q.front(); q.pop();
                 ret.back().push_back(node->val);
                 if (node->left) q.push(node->left);
                 if (node->right) q.push(node->right);
             }
         }
+
         return ret;
     }
 };
