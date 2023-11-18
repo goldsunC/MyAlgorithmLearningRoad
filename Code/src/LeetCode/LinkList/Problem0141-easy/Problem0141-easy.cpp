6a58243f53270680fb0437d4c30c79cf7d9ef034
yangning <2428022854@qq.com>
2023-11-18 18:31:38 +0800

    commit P0141

---
+++ Code/src/LeetCode/LinkList/Problem0141-easy/Problem0141-easy.cpp
@@ -0,0 +1,21 @@
+//
+// Created by 阳寜 on 2023/11/18.
+//
+#include<bits/stdc++.h>
+
+typedef long long ll;
+using namespace std;
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+// 快慢指针
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+
+    }
+};
