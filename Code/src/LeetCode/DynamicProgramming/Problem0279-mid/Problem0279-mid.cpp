a7ca0267d24216f51672ed2163281d8a257bc5da
yangning <2428022854@qq.com>
2023-11-21 21:26:34 +0800

    P0279

--- Code/src/LeetCode/DynamicProgramming/Problem0279-mid/Problem0279-mid.cpp
+++ Code/src/LeetCode/DynamicProgramming/Problem0279-mid/Problem0279-mid.cpp
@@ -8,9 +8,14 @@ using namespace std;
 class Solution {
 public:
     int numSquares(int n) {
-
+        vector<int> opt(n + 1);
+        for (int i = 1; i <= n; ++i) {
+            int minn = INT_MAX;
+            for (int j = 1; j*j <= i; ++j) {
+                minn = min(minn, opt[i - j*j]);
+            }
+            opt[i] = minn + 1;
+        }
+        return opt[n];
     }
 };
-int main() {
-    return 0;
-}
